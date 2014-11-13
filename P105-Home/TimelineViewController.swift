d30d77fb725a06a8f46de7bbea276e005199503d
Darrin Henein <darrinhenein@gmail.com>
2014-11-13 14:09:50 -0500

    Added timeline prototype view

---
+++ P105-Home/TimelineViewController.swift
@@ -0,0 +1,73 @@
+//
+//  TimelineViewController.swift
+//  P105-Home
+//
+//  Created by Darrin Henein on 2014-11-13.
+//  Copyright (c) 2014 Darrin Henein. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class TimelineViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+
+    @IBOutlet var containerView: UIView! = nil
+    
+    var webView: WKWebView?
+    
+    
+    @IBAction func refreshWasPressed(sender: AnyObject) {
+        self.webView!.evaluateJavaScript("refreshFeed()", completionHandler: nil)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        var config = WKWebViewConfiguration()
+        config.userContentController.addScriptMessageHandler(self, name: "bridge")
+        
+        self.webView = WKWebView(frame: self.view.frame, configuration: config)
+        self.webView?.navigationDelegate = self
+        self.view = self.webView!
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let fullURL:NSURL! = NSURL(string: "https://people.mozilla.org/~dhenein/labs/mobile-feed")
+        let req:NSURLRequest! = NSURLRequest(URL: fullURL)
+        
+        self.webView!.loadRequest(req)
+
+    }
+    
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        let sentData = message.body as NSDictionary
+        let status : String! = sentData["status"] as String
+        if status == "loaded" {
+            
+            var syncUser: Credentials!
+            syncUser = Login().getKeychainUser(Login().getUsername())
+            
+            self.webView!.evaluateJavaScript("storeUser('\(syncUser.username!)', '\(syncUser.password!)')", completionHandler: nil)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
